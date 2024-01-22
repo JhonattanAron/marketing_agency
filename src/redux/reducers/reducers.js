2aaf342e724cccdb1c3a7788f8375ee0b2eed8d8
AronLaptop <aron.cachago@gmail.com>
2024-01-22 11:52:57 -0500

    4.4.7

---
+++ src/redux/reducers/reducers.js
@@ -0,0 +1,10 @@
+const counterReducer = (state = { count: 0 }, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + 1 };
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
