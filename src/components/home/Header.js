3982c14b1b03cc3f38f5e32ae340cc45f5e9da78
Aron Cachago <115675860+JhonattanAron@users.noreply.github.com>
2024-03-19 22:13:00 -0500

    2.5.9

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -51,7 +51,7 @@ function Header(props) {
               </p>
               <div className="mt-8 flex gap-x-4 sm:justify-center">
                 <a
-                  href="/"
+                  href="/contacto"
                   className="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
                 >
                   Empezar
@@ -60,7 +60,7 @@ function Header(props) {
                   </span>
                 </a>
                 <a
-                  href="/"
+                  href="/contacto"
                   className="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-1 ring-white-transparent bg-white-transparent hover:ring-gray-900/20"
                 >
                   Demo en vivo
